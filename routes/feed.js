2247b33dc656b8c97e595797a9647f51be899a72
Aryan Kushwaha <aryan.mnitj@gmail.com>
2024-10-02 23:45:38 +0530

    added dev till update post

--- routes/feed.js
+++ routes/feed.js
@@ -5,11 +5,16 @@ const router = express.Router();
 
 router.get('/posts',feedController.getPosts);
 
+router.get('/post/:postId', feedController.getPost);
+
 router.post('/post',
     [body('title').trim().isLength({min : 5}),
     body('content').trim().isLength({min : 5})],
     feedController.createPost);
 
-router.get('/post/:postId', feedController.getPost);
+router.put('/post/:postId',
+    [body('title').trim().isLength({min : 5}),
+    body('content').trim().isLength({min : 5})],
+    feedController.updatePost)
 
 module.exports = router;
