fae3af02240cf735b11f754cb4f759a16fc5120f
Aryan Kushwaha <aryan.mnitj@gmail.com>
2024-10-02 19:45:35 +0530

    done till uploading image

--- app.js
+++ app.js
@@ -1,16 +1,55 @@
+const path = require ('path');
 const express = require('express');
 const bodyParser = require ('body-parser');
 const feedRoutes = require ('./routes/feed');
 const mongoose = require ('mongoose');
+const multer = require ('multer');
+const { v4: uuidv4 } = require('uuid');
+
 const app = express();
+
+const fileStorage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'images');
+    },
+    filename: function(req, file, cb) {
+        cb(null, uuidv4()+ file.originalname)
+    }
+});
+
+const fileFilter = (req, file, cb) =>{
+    if(file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg'){
+        cb(null, true);
+    }
+    else{
+        cb(null, false);
+    }
+}
 //app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use (bodyParser.json()); //application/json
+
+app.use (multer({
+    storage : fileStorage,
+    fileFilter : fileFilter
+}).single('image'))
+
 app.use ((req, res ,next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next ();
 })
+
+app.use('/images',express.static(path.join(__dirname, 'images')));
+
 app.use('/feed', feedRoutes);
-mongoose.connect ('')
-app.listen(8080);
+app.use ((error,req,res,next)=>{
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json ({message : message});
+
+})
+
+mongoose.connect ('mongodb+srv://aryanmnitj:Arykush1$U+762817@cluster0.dvnql.mongodb.net/messages?retryWrites=true&w=majority&appName=Cluster0') .then ( result => {
+    app.listen(8080);
+}).catch(err => console.log(err));
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,34 +1,70 @@
 const express = require('express');
 const {validationResult} = require ('express-validator');
+const Post = require('../models/post');
 
 exports.getPosts = (req,res,next) =>{
-res.status(200).json ({
-    posts: [
-        {_id : '1', title : 'First Post', content : 'This is the first post', creator : {name : 'Aryan'}, createdAt : new Date ()},
-        {_id : '2', title : 'Second Post', content : 'This is the second post', creator : {name : 'Mayank'}, createdAt : new Date ()}, 
-        {_id : '3', title : 'Third Post', content : 'This is the third post', creator : {name : 'Satyam'}, createdAt : new Date ()},
-        {_id : '4', title : 'Final Post', content : 'This is the final post', creator : {name : 'Ayush'}, createdAt : new Date ()}]
-})
+    Post.find()
+    .then(posts=>{
+        res.status(200).json({posts : posts})
+    })
+    .catch(err => {
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    })
 };
 
 exports.createPost = (req,res,next) =>{
     const errors = validationResult (req);
     if(!errors.isEmpty()){
-        return res.status(422).json({
-            message : 'validation failed',
-            erorrs : errors.array()
-        })
+        const error = new Error('validation failed');
+        error.statusCode = 422;
+        throw error ;
     }
+    if (!req.file){
+        const error = new Error('No image provided');
+        error.statusCode = 422;
+        throw error ;
+    }
+    const imageUrl = req.file.path.replace("\\" ,"/");
     const title = req.body.title;
     const content = req.body.content;
-    res.status(201).json({
-        message : 'Post created successfully',
-        post : 
-        {_id : new Date ().toISOString(),
-          title : title , 
-          content : content,
-          creator : {name : 'Kumar'},
-          createdAt : new Date ()
+    const post = new Post ({
+        title : title , 
+        imageUrl : imageUrl,
+        content : content,
+        creator : {name : 'Kumar'},
+    })
+    post.save()
+    .then (result => 
+        {res.status(201).json({
+            message : 'Post created successfully',
+            post : result
+        })})
+    .catch ((err)=>{
+        if(!err.statusCode){
+            err.statusCode = 500;
         }
+        next(err);
     })
 }
+exports.getPost = (req,res,next) =>{
+    const postId = req.params.postId;
+    Post.findById(postId)
+    .then(post=>{
+        if(!post){
+            const error = new Error ('Could not find post.');
+            error.statusCode = 404;
+            throw error;
+        }
+        res.status(200).json({post : post})
+    })
+    .catch(err => {
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    })
+};
+    
